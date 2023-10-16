2385d706204c54e7673bccb0d09c603a5ac6af59
RebaiRafika <147846850+RebaiRafika@users.noreply.github.com>
2023-10-16 18:49:43 +0100

    Add files via upload

---
+++ TP1SOA/rebairafikaSOA/tp2SOA/src/service/BanqueService.java
@@ -0,0 +1,12 @@
+package service;
+
+public class BanqueService {
+	public double conversion(double montant){
+		return(montant * 10.6);
+		}
+	public Compte getCompte{ 
+		
+	}
+	}
+
+
---
+++ TP1SOA/rebairafikaSOA/tp2SOA/src/service/Compte.java
@@ -0,0 +1,13 @@
+package service;
+
+public class Compte {
+int code;
+double solde;
+String Datecreation;
+public class
+public static void main() {
+	
+}
+
+
+}
